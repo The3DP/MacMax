92baf6622beb89ac846f7d23b7b7dd5f06007dd5
Bit77 <daren@markandtraci.com>
2025-08-12 08:47:44 -0500

    Update Level3.py

--- Level3.py
+++ Level3.py
@@ -1,1 +1,7 @@
-
+python3 -c "
+import multiprocessing
+def burn(n): [x**2 for x in range(10**6)]
+procs = [multiprocessing.Process(target=burn, args=(i,)) for i in range(multiprocessing.cpu_count())]
+[p.start() for p in procs]
+[p.join() for p in procs]
+"
