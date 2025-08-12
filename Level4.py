d3f5375707ea10f5e26a4675dc17c1e5c1dcd22e
Bit77 <daren@markandtraci.com>
2025-08-12 08:48:01 -0500

    Update Level4.py

--- Level4.py
+++ Level4.py
@@ -1,1 +1,7 @@
-
+python3 -c "
+import multiprocessing
+def stress(): 
+    while True: [x**2 for x in range(1000)]
+procs = [multiprocessing.Process(target=stress) for _ in range(multiprocessing.cpu_count())]
+[p.start() for p in procs]
+"
